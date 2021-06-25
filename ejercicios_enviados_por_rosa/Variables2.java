0146224d93051e7507068931a991ca10d7dbb3ac
RosaBitxi <82233228+RosaBitxi@users.noreply.github.com>
2021-06-25 10:50:09 +0200

    Update Variables2.java

--- ejercicios_enviados_por_rosa/Variables2.java
+++ ejercicios_enviados_por_rosa/Variables2.java
@@ -16,39 +16,40 @@ public class Variables2 {
 
 	/*
 	 * Determina cual es el tipo de dato del resultado de las siguientes expresiones
-	 * aritm�ticas:
+	 * aritméticas:
 	 *
 	 * (Ver figura 2)
 	 * 
 	 */
 
-	private int a_ = i + c;
-	private float b_ = f - c;
-	private double c_ = d + f;
-	private double d_ = d + i;
-	private float e_ = i / f;
-	private int f_ = s + j;
-	private double g_ = d + j;
-	private int h_ = s * c;
-	private double i_ = d + c;
-	private int j_ = b + c;
-	private float k_ = b / c + s;
-	private int l_ = c + c; // Porque es aritm�tica, si pongo char concatenar�a.
-	private double m_ = i + ln + d;
-	private long n_ = ln + c;
-	private int o_ = s / j;
-	private double p_ = 5.2 / j; // Porque para float ser�a 5.2f
-	private double q_ = i * f * 2.5;
-	private float r_ = ln * f * 2.5F;
-	private long s_ = j - 4L;
-	private float t_ = j + 4L * 2.5F;
-	private double u_ = b+2.5*i*35F;
-	private int v_ = 'a'+b;
-	private int w_ = 'a'+c;
-	private int x_ = c+2;
-	private long y_ = c-ln/2;
-	private double z_ = 2 / i +2.0 / j;
+	private int a_ = i + c; // sii
+	private float b_ = f - c; // ¿?
+	private double c_ = d + f; // sii
+	private double d_ = d + i; // sii
+	private float e_ = i / f; // sii
+	private int f_ = s + j; // sii
+	private double g_ = d + j; //sii
+	private int h_ = s * c; // sii
+	private double i_ = d + c; // sii
+	private int j_ = b + c; // sii
+	private float k_ = b / c + s; // ¿?
+	private int l_ = c + c; // Porque es aritmética, si pongo char concatenaría. sii
+	private double m_ = i + ln + d; // sii
+	private long n_ = ln + c; // sii
+	private int o_ = s / j; // sii
+	private double p_ = 5.2 / j; // Porque para float sería 5.2f sii
+	private double q_ = i * f * 2.5; // sii
+	private float r_ = ln * f * 2.5F; //sii
+	private long s_ = j - 4L; // sii
+	private float t_ = j + 4L * 2.5F; // sii
+	private double u_ = b+2.5*i*35F; // sii
+	private int v_ = 'a'+b; // sii
+	private int w_ = 'a'+c; // sii
+	private int x_ = c+2; // sii
+	private long y_ = c-ln/2; // ¿?
+	private double z_ = 2 / i +2.0 / j; // sii
 	
+	// Las soluciones me dice que cuando hai char, siempre son int la solución. Por eso pongo esas interrogaciones. A la tarde lo miro y te digo
 	public static void main(String[] args) {
 
 	}
