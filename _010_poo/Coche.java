be9c1db8daadda8142272ad842b7d7b11826d449
Charlie Martinez <39219855+charliemartinez@users.noreply.github.com>
2021-06-29 08:58:30 +0200

    Add files via upload

--- _010_poo/Coche.java
+++ _010_poo/Coche.java
@@ -3,49 +3,70 @@ package _010_poo;
 public class Coche {
 
 	private int ruedas, largo, ancho, motor, pesoPlataforma;
-	String color;
-	int pesoTotal;
-	boolean asientosCuero, aireAcondicionado;	
-
-	// método constructor:
+	private String color;
+	private int pesoTotal;
+	private boolean asientosCuero, aireAcondicionado;
 
 	/*
-	 * Método especial que se encarga de dar un estado inicial a nuestro objeto.
+	 * Constructor: Método especial que se encarga de dar un estado inicial a
+	 * nuestro objeto.
 	 * 
 	 */
 
-	public Coche() {
+	public Coche() { // CONSTRUCTOR
 		ruedas = 4;
 		largo = 2000;
 		ancho = 300;
 		motor = 1600;
-		pesoPlataforma = 500; 
+		pesoPlataforma = 500;
 	};
-	
-	// Métodos getters
-	
-	public String mostrarMedidas() {
-		
-		return "El coche mide "+largo+" cm. de largo.\ny "+ancho+"cm. de ancho.";
+
+	public String mostrarDatosGenerales() { // GETTER
+
+		return "El coche mide " + largo + " cm. de largo y " + ancho + "cm. de ancho.\n" + "Tiene " + ruedas
+				+ " ruedas.\n" + "Su plataforma pesa: " + pesoPlataforma + " kg.";
+
+	}
+
+	public void definirColor(String color) { // SETTER
+		this.color = color;
+	}
+
+	public String mostrarColor() { // SETTER
+		return "El coche es de color " + color + ".";
+	}
+
+	//SETTER
+	public void definirAsientos(String asientosCuero) {
+		if (asientosCuero == "si") {
+			this.asientosCuero = true;
+		} else {
+			this.asientosCuero = false;
+		}
 	}
 	
-	public String mostrarRuedas(){
-		return "El coche tiene "+ruedas+" ruedas.";
-	}
-	
-	public String mostrarPesoPlataforma() {
-		
-		return "La plataforma del coche pesa: "+pesoPlataforma+" kg.";
-	}
-	// Método setters
-	
-	public void definirColor() {
-		color="azul";
-	}
-	
-	// Método setter
-	
-	public String mostrarColor() {
-		return "El auto es de color "+color+"."; 
+	//GETTER
+	public String mostrarAsientos() {
+		if(asientosCuero==true) {
+			return"El coche tiene asientos de cuero.";
+		}
+		else {
+			return"El coche tiene asientos de serie.";
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- _010_poo/UsoCoche.java
+++ _010_poo/UsoCoche.java
@@ -5,15 +5,15 @@ public class UsoCoche {
 	public static void main(String[] args) {
 	
 		Coche miCoche = new Coche(); // Instanciar una clase: ejemplar de clase
-		
-		miCoche.definirColor();
-		
+		miCoche.definirColor("celeste");
 		System.out.println(miCoche.mostrarColor());
 		
 		Coche otroCoche = new Coche();
-		
+		otroCoche.definirColor("blanco");
 		System.out.println(otroCoche.mostrarColor());
-		
+		otroCoche.definirAsientos("si");
+		System.out.println(otroCoche.mostrarAsientos());
+			
 	}
 
 }
