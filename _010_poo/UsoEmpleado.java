96b15c2211bd33613b611fb642e78bd98b3fc718
Charlie Martinez <39219855+charliemartinez@users.noreply.github.com>
2021-06-29 10:32:56 +0200

    Add files via upload

--- _010_poo/UsoEmpleado.java
+++ _010_poo/UsoEmpleado.java
@@ -1,6 +1,7 @@
 package _010_poo;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 // Modelar Figura1.PNG en un único fichero con varias clases
 
@@ -16,13 +17,49 @@ class Empleado {
 	
 	private String nombre;
 	private double sueldo;
-	private Date altaContrato;
+	private Date fechaAltaContrato;
 	 
 
 	public Empleado(String nombre, double sueldo, int anio, int mes, int dia) {
-
+		this.nombre=nombre;
+		this.sueldo=sueldo;
+		//mes-1 porque comienza en cero
+		GregorianCalendar calendario=new GregorianCalendar(anio,mes-1,dia);
+		//devuelve la fecha
+		this.fechaAltaContrato=calendario.getTime();
+		/* getTime, en la parte de la API de Class GregorianCalendar
+		 * no aparece, ya que se trata de un método heredado
+		 * de java.util.Calendar.
+		 * 
+		 */
 	}
 	
-
+	//GETTER
+	public String mostrarNombre() {
+		return this.nombre;
+	}
+	
+	//GETTER
+	public double mostrarSueldo() {
+		return this.sueldo;
+	}
+	
+	//GETTER
+	public Date mostrarFechaAltaContrato() {
+		return this.fechaAltaContrato;
+	}
+	
+	/*
+	 * No es necesario poner this cuando no hay
+	 * variable homónimas, sin embargo prefiero
+	 * utilizarlo igual ya que me ayuda a ver
+	 * con más claridad el código.
+	 * 
+	 */
+	
+	public void subeSueldo(double porcentaje) {
+		double aumento=sueldo*porcentaje/100;
+		sueldo+=aumento;
+	}
 
 }
