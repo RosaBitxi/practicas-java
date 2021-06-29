b1bdd8e082852cbf510cea76449339893f6bd8f3
Charlie Martinez <39219855+charliemartinez@users.noreply.github.com>
2021-06-29 10:09:48 +0200

    Add files via upload

---
+++ _010_poo/UsoEmpleado.java
@@ -0,0 +1,28 @@
+package _010_poo;
+
+import java.util.Date;
+
+// Un único fichero con varias clases
+
+public class UsoEmpleado {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+class Empleado {
+	
+	private String nombre;
+	private double sueldo;
+	private Date altaContrato;
+	 
+
+	public Empleado(String nombre, double sueldo, int anio, int mes, int dia) {
+
+	}
+	
+
+
+}
